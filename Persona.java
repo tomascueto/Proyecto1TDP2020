c459f9eaa14e4f99d689655338547784c04e0462
tomascueto <tomasco98@gmail.com>
2020-09-25 08:21:09 -0300

    Commit inicial

---
+++ Persona.java
@@ -0,0 +1,24 @@
+
+public class Persona {
+
+	private String nombre;
+	
+	
+	public Persona(String nom) {
+		
+		nombre = nom;
+		
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+
+	
+	
+}
---
+++ Prueba.java
@@ -0,0 +1,7 @@
+
+public class Prueba {
+	public static void main (String [] args) {
+		Persona p1 = new Persona("Tomas");
+		System.out.println("Nombre: "+p1.getNombre());		
+	}
+}
